d2cb2f50bb23c961a53df2d850e70ca89ec206a2
Dario Sestero <dsestero@gmail.com>
2016-11-07 11:25:11 +0100

    Added support for RedHat/CentOS distributions

--- manifests/install_config.pp
+++ manifests/install_config.pp
@@ -12,37 +12,35 @@
 # none
 #
 define java::install_config ($java_default_version, $java_version = $title,) {
-  if $java_default_version != undef and $java_version == $java_default_version {
-    case $::operatingsystem {
-      'Ubuntu' : {
-        case $::operatingsystemrelease {
-          '10.04' : {
-            $javapkg = "openjdk-${java_version}-jdk"
-            $javadir = "/usr/lib/jvm/java-${java_version}-openjdk/jre/bin/java"
-          }
-          '12.04' : {
-            $javapkg = "openjdk-${java_version}-jdk"
-            $javadir = "/usr/lib/jvm/java-${java_version}-openjdk-${::architecture}/jre/bin/java"
-          }
-          default : {
-            fail("The ${module_name} module is not supported on ${::operatingsystem} release ${::operatingsystemrelease}"
-            )
-          }
+  case $::operatingsystem {
+    'Ubuntu'           : {
+      case $::operatingsystemrelease {
+        '10.04' : {
+          $javapkg = "openjdk-${java_version}-jdk"
+          $javadir = "/usr/lib/jvm/java-${java_version}-openjdk/jre/bin/java"
+        }
+        '12.04' : {
+          $javapkg = "openjdk-${java_version}-jdk"
+          $javadir = "/usr/lib/jvm/java-${java_version}-openjdk-${::architecture}/jre/bin/java"
+        }
+        default : {
+          fail("The ${module_name} module is not supported on ${::operatingsystem} release ${::operatingsystemrelease}")
         }
       }
-      'CentOS', 'RedHat' : {
-            $javapkg = "java-1.${java_version}.0-openjdk"
-            $javadir = "/usr/lib/jvm/jre-1.${java_version}.0-openjdk.${::architecture}/bin/java"
-      }
-      default  : {
-        fail("The ${module_name} module is not supported on an ${::operatingsystem} distribution."
-        )
-      }
     }
+    'CentOS', 'RedHat' : {
+      $javapkg = "java-1.${java_version}.0-openjdk"
+      $javadir = "/usr/lib/jvm/jre-1.${java_version}.0-openjdk.${::architecture}/bin/java"
+    }
+    default            : {
+      fail("The ${module_name} module is not supported on an ${::operatingsystem} distribution.")
+    }
+  }
 
-    package { $javapkg:
-      ensure => installed,
-    } ->
+  package { $javapkg:
+    ensure => installed,
+  } ->
+  if $java_default_version != undef and $java_version == $java_default_version {
     exec { 'set_java':
       command => "/usr/sbin/update-alternatives --set java ${javadir}",
       unless  => "ls -l /etc/alternatives/java | grep ${javadir}",
